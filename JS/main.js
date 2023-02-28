83e4204b3d746e4aa610fb171635ae1fd0a85f45
Priscila <priscilazeniquel@hotmail.com>
2023-02-28 12:05:36 -0300

    Prueba con DOM

--- JS/main.js
+++ JS/main.js
@@ -1,69 +1,90 @@
-const carrito = [];
+let filtroBuzos = document.getElementById("buzos");
+filtroBuzos.addEventListener("click", mostrarCategoria);
+ function mostrarCategoria() {
+    const resultado = productos.filter((producto) => producto.categoria.includes("Buzos"));
+    console.log(resultado);
+}
 
-let prendaSeleccionada = " "
-let objetoSeleccionado = " "
-let cantidadSeleccionada = 0
-let seguirComprando = true;
-let subtotal = 0;
+mostrarCategoria();
 
-do {
-    const productosNombre = productos.map(productos => productos.nombre);
-    alert("Bienvenida a la tienda de Alaska.\nLas prendas en stock son :\n" +productosNombre.join(", "));
-    prendaSeleccionada = prompt("Que prenda queres comprar?\n" + productosNombre.join(", ")).toLowerCase();
-    objetoSeleccionado = productos.find(prod => prod.nombre.toLowerCase() == prendaSeleccionada);
-    alert("El color es: " + objetoSeleccionado.color + "\nEl talle es: " + objetoSeleccionado.talle + "\nEl precio es: $" + objetoSeleccionado.precio);
-    cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"));
-    if (cantidadSeleccionada > objetoSeleccionado.cantidad) {
-        alert("No contamos con el stock ingresado, por favor ingrese otra cantidad")
-        cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"))
-    } else {
-        alert("Queres comprar " + cantidadSeleccionada + " " + objetoSeleccionado.nombre);
-        subtotal += cantidadSeleccionada * objetoSeleccionado.precio;
-        objetoSeleccionado.cantidad = cantidadSeleccionada;
-        carrito.push(objetoSeleccionado);
-    }
 
-    seguirComprando = confirm("Queres seguir comprando?");
 
-    if (seguirComprando == true) {
+
+
+
+
+
+
+
+
+
+
+// const carrito = [];
+
+// let prendaSeleccionada = " "
+// let objetoSeleccionado = " "
+// let cantidadSeleccionada = 0
+// let seguirComprando = true;
+// let subtotal = 0;
+
+// do {
+//     const productosNombre = productos.map(productos => productos.nombre);
+//     alert("Bienvenida a la tienda de Alaska.\nLas prendas en stock son :\n" +productosNombre.join(", "));
+//     prendaSeleccionada = prompt("Que prenda queres comprar?\n" + productosNombre.join(", ")).toLowerCase();
+//     objetoSeleccionado = productos.find(prod => prod.nombre.toLowerCase() == prendaSeleccionada);
+//     alert("El color es: " + objetoSeleccionado.color + "\nEl talle es: " + objetoSeleccionado.talle + "\nEl precio es: $" + objetoSeleccionado.precio);
+//     cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"));
+//     if (cantidadSeleccionada > objetoSeleccionado.cantidad) {
+//         alert("No contamos con el stock ingresado, por favor ingrese otra cantidad")
+//         cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"))
+//     } else {
+//         alert("Queres comprar " + cantidadSeleccionada + " " + objetoSeleccionado.nombre);
+//         subtotal += cantidadSeleccionada * objetoSeleccionado.precio;
+//         objetoSeleccionado.cantidad = cantidadSeleccionada;
+//         carrito.push(objetoSeleccionado);
+//     }
+
+//     seguirComprando = confirm("Queres seguir comprando?");
+
+//     if (seguirComprando == true) {
         
-    } else {
-        mostrarCarrito();
-        alert("El subtotal de tu compra es de: $"+subtotal);
-    }
+//     } else {
+//         mostrarCarrito();
+//         alert("El subtotal de tu compra es de: $"+subtotal);
+//     }
 
-} while (seguirComprando);
-
-
-
-function mostrarCarrito () {
-    let mensaje = "Estos son los productos de tu carrito\n";
-    carrito.forEach((producto) => {
-        mensaje += producto.nombre+ ": "+producto.cantidad+"\n";
-    })
-    alert(mensaje);
-}
+// } while (seguirComprando);
 
 
-let envio = prompt("Elegi ENVIO A DOMICILIO o RETIRAR POR EL LOCAL").toLowerCase()
-let datosEnvio = ""
-let datosRetiro = ""
-let totalAbonar = subtotal + 2000
 
-function Envio() {
+// function mostrarCarrito () {
+//     let mensaje = "Estos son los productos de tu carrito\n";
+//     carrito.forEach((producto) => {
+//         mensaje += producto.nombre+ ": "+producto.cantidad+"\n";
+//     })
+//     alert(mensaje);
+// }
 
-    if ((envio == "envio a domicilio") && (subtotal >= 10000)) {
-        alert("El envio a domicilio es gratis")
-        alert("El total abonar es $" + subtotal)
-        datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
-    } else if ((envio == "envio a domicilio") && (subtotal < 10000)) {
-        alert("El costo del envio a domicilio es de $2000")
-        alert("El total abonar es $" + totalAbonar)
-        datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
-    } else {
-        datosRetiro = prompt("Tu pedido estara disponible para retirar en los proximos 10 dias habiles, te enviaremos un mensaje cuando este disponible. Dejanos tu nombre completo y telefono")
-    }
 
-}
+// let envio = prompt("Elegi ENVIO A DOMICILIO o RETIRAR POR EL LOCAL").toLowerCase()
+// let datosEnvio = ""
+// let datosRetiro = ""
+// let totalAbonar = subtotal + 2000
 
-Envio();
+// function Envio() {
+
+//     if ((envio == "envio a domicilio") && (subtotal >= 10000)) {
+//         alert("El envio a domicilio es gratis")
+//         alert("El total abonar es $" + subtotal)
+//         datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
+//     } else if ((envio == "envio a domicilio") && (subtotal < 10000)) {
+//         alert("El costo del envio a domicilio es de $2000")
+//         alert("El total abonar es $" + totalAbonar)
+//         datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
+//     } else {
+//         datosRetiro = prompt("Tu pedido estara disponible para retirar en los proximos 10 dias habiles, te enviaremos un mensaje cuando este disponible. Dejanos tu nombre completo y telefono")
+//     }
+
+// }
+
+// Envio();
--- JS/productos.js
+++ JS/productos.js
@@ -1,21 +1,22 @@
 const productos = []; //Array que contiene los objetos (las prendas)
 
 class Prendas { //Clase constructora con la cual arme los objetos
-    constructor(id, nombre, color, talle, cantidad, precio) {
+    constructor(id, nombre, color, talle, cantidad, precio, categoria) {
         this.id = id
         this.nombre = nombre;
         this.color = color;
         this.talle = talle;
         this.cantidad = cantidad;
         this.precio = precio;
+        this.categoria = categoria;
     }
 }
 
-productos.push(new Prendas("id: 1", "Buzo Anorak", "Verde Lima", "XL", 10, 10000))
-productos.push(new Prendas("id: 2", "Jogger Washed", "Negro", "48", 5, 12000))
-productos.push(new Prendas("id: 3", "Camisa Firenze", "Blanco", "M", 7, 9000))
-productos.push(new Prendas("id: 4", "Remera Roma", "Blanco", "S", 10, 6000))
-productos.push(new Prendas("id: 5", "Buzo Capa", "Rosa", "S", 10, 11000))
-productos.push(new Prendas("id: 6", "Jean Boyfriend", "Azul", "42", 8, 13000))
-productos.push(new Prendas("id: 7", "Camisa Bow", "Blanco", "M", 12, 12000))
-productos.push(new Prendas("id: 8", "Remera Nudos", "Negro", "S", 10, 7000))
+productos.push(new Prendas("id: 1", "Buzo Buda", "Blanco", "XL", 10, 10000, "Buzos"))
+productos.push(new Prendas("id: 2", "Jogger Washed", "Celeste", "48", 5, 12000, "Pantalones"))
+productos.push(new Prendas("id: 3", "Camisa Firenze", "Amarillo", "M", 7, 9000, "Camisas"))
+productos.push(new Prendas("id: 4", "Remera Roma", "Blanco", "S", 10, 6000, "Remeras"))
+productos.push(new Prendas("id: 5", "Hoodie Power", "Negro", "S", 10, 11000, "Buzos"))
+productos.push(new Prendas("id: 6", "Jean Boyfriend", "Azul", "42", 8, 13000, "Pantalones"))
+productos.push(new Prendas("id: 7", "Camisa Bow", "Blanco", "M", 12, 12000, "Camisas"))
+productos.push(new Prendas("id: 8", "Remera Otis", "Rosa", "S", 10, 7000, "Remeras"))
