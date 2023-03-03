44e8d5d58426943472d45dec4fae30a9c3c12c29
Priscila <priscilazeniquel@hotmail.com>
2023-03-03 15:46:25 -0300

    Actualizacion carrito

--- JS/data.js
+++ JS/data.js
@@ -14,20 +14,20 @@ class Prendas { //Clase constructora con la cual arme los objetos
     }
 }
 
-productos.push(new Prendas("id: 1", "buzo buda", "blanco", "s", 10, 10000, "buzos", "./multimedia/buzo-buda.jpg", "./multimedia/buzo-buda2.jpg"))
+productos.push(new Prendas("id:1", "buzo buda", "blanco", "s", 10, 10000, "buzos", "./multimedia/buzo-buda.jpg", "./multimedia/buzo-buda2.jpg"))
 
-productos.push(new Prendas("id: 2", "jogger washed", "celeste", "48", 5, 12000, "pantalones", "./multimedia/jogger-washed.jpg", "./multimedia/jogger-washed2.jpg"))
+productos.push(new Prendas("id:2", "jogger washed", "celeste", "48", 5, 12000, "pantalones", "./multimedia/jogger-washed.jpg", "./multimedia/jogger-washed2.jpg"))
 
-productos.push(new Prendas("id: 3", "camisa firenze", "amarillo", "l", 7, 9000, "camisas", "./multimedia/camisa-firenze.jpg", "./multimedia/camisa-firenze2.jpg"))
+productos.push(new Prendas("id:3", "camisa firenze", "amarillo", "l", 7, 9000, "camisas", "./multimedia/camisa-firenze.jpg", "./multimedia/camisa-firenze2.jpg"))
 
-productos.push(new Prendas("id: 4", "remera roma", "blanco","m", 10, 6000, "remeras", "./multimedia/remera-roma.jpg", "./multimedia/remera-roma2.jpg"))
+productos.push(new Prendas("id:4", "remera roma", "blanco","m", 10, 6000, "remeras", "./multimedia/remera-roma.jpg", "./multimedia/remera-roma2.jpg"))
 
-productos.push(new Prendas("id: 5", "hoodie power", "negro", "xl", 10, 11000, "buzos", "./multimedia/hoodie-power.jpg", "./multimedia/hoodie-power2.jpg"))
+productos.push(new Prendas("id:5", "hoodie power", "negro", "xl", 10, 11000, "buzos", "./multimedia/hoodie-power.jpg", "./multimedia/hoodie-power2.jpg"))
 
-productos.push(new Prendas("id: 6", "jean boyfriend", "azul", "46", 8, 13000, "pantalones", "./multimedia/jean-boyfriend2.jpg", "./multimedia/jean-boyfriend.jpg"))
+productos.push(new Prendas("id:6", "jean boyfriend", "azul", "46", 8, 13000, "pantalones", "./multimedia/jean-boyfriend2.jpg", "./multimedia/jean-boyfriend.jpg"))
 
-productos.push(new Prendas("id: 7", "camisa bow", "blanco", "s", 12, 12000, "camisas", "./multimedia/camisa-bow.jpg", "./multimedia/camisa-bow2.jpg"))
+productos.push(new Prendas("id:7", "camisa bow", "blanco", "s", 12, 12000, "camisas", "./multimedia/camisa-bow.jpg", "./multimedia/camisa-bow2.jpg"))
 
-productos.push(new Prendas("id: 8", "remera otis", "rosa", "l", 10, 7000, "remeras", "./multimedia/remera-otis.jpg", "./multimedia/remera-otis2.jpg"))
+productos.push(new Prendas("id:8", "remera otis", "rosa", "l", 10, 7000, "remeras", "./multimedia/remera-otis.jpg", "./multimedia/remera-otis2.jpg"))
 
 
--- JS/main.js
+++ JS/main.js
@@ -1,104 +1,13 @@
-// Construccion del carrito
-
-const contenedorCarrito = document.getElementById("mostrarCarrito");
-const divCarrito = document.createElement("div");
-divCarrito.classList.add("modal");
-divCarrito.innerHTML = `
-    <div class="resumenCarrito">
-        <button class="cerrar">X</button>
-        <p>Producto</p>
-        <p class="precio">Precio unitario</p>
-        <p class="cantidad">Cantidad</p>
-    </div>
-
-    <div class="compraFinal">
-        <p class="resumen">RESUMEN DE COMPRA</p>
-        <p class="subtotal">Subtotal</p>
-        <p class="envio">Envio</p>
-        <p class="total"><b>TOTAL</b></p>
-        <button class="comprar">INICIAR COMPRA</button>
-    </div> `
-
-contenedorCarrito.appendChild(divCarrito);
-
-// Abrir y cerrar carrito
-
-// const abrirCarrito = document.getElementsByClassName("carrito");
-// const cerrarCarrito = document.getElementsByClassName("cerrar");
-// const modal = document.getElementsByClassName("modal");
-
-
-// abrirCarrito.addEventListener("click", function() {
-//     modal.style.display = "block";
-// });
-
-
-// cerrarCarrito.addEventListener("click", function() {
-//     modal.style.display = "none";
-// });
-
-// modal.addEventListener("click", function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";    
-//     }
-// });
-
-
-// filtro del navegador por categorias
-
-let filtroPrendas = document.getElementsByClassName("btnFiltro");
-
-for (const filtro of filtroPrendas) {
-    filtro.addEventListener("click", mostrarCategoria);
-}
-
- function mostrarCategoria(e) {
-    e.preventDefault()
-    const categoria = e.target.id;  
-    const resultado = productos.filter(producto => producto.categoria.includes(categoria));
-    contenedor.innerHTML= '';
-    resultado.forEach((producto) => {
-        const divProd = document.createElement("div");
-        divProd.classList.add("card");
-        divProd.innerHTML = `
-    <div class="imgContenedor">
-        <img src="${producto.img}" alt="${producto.nombre}">
-        <img src="${producto.img2}" alt="${producto.nombre}">
-    </div>
-
-    <div class="infoContenedor">
-        <div class="infoUno">
-            <p class=""titulo>${producto.nombre}</p>
-            <p><b>$${producto.precio}</b></p>
-        </div>
-
-        <div class="detalleContenedor">
-            <p class="color"><b>color:</b> ${producto.color} </p>
-        </div>
-
-        <div>
-            <p class="talle"><b>talle:</b> ${producto.talle}</p>
-        </div>
-    </div>
-
-        <input type="button" value="COMPRAR" id="comprar" class="btnCarrito">`
-
-            contenedor.appendChild(divProd);
-
-            const btnCarrito = document.getElementById("comprar");
-
-            btnCarrito.addEventListener("click", () => {
-                agregarAlCarrito(producto.id)
-            })
-        });
-}
-
-
 // Construccion de las Card
 
 const contenedor = document.getElementById("contenedor");
+const modal = document.getElementById("modal");
+let carrito = [];
 
-productos.forEach((producto) => {
+function mostrarProductos(array) {
+
+    contenedor.innerHTML= '';
+    array.forEach((producto) => {
     const divProd = document.createElement("div");
     divProd.classList.add("card");
     divProd.innerHTML = `
@@ -109,6 +18,7 @@ productos.forEach((producto) => {
     </div>
 
     <div class="infoContenedor">
+
         <div class="infoUno">
             <p class=""titulo>${producto.nombre}</p>
             <p><b>$${producto.precio}</b></p>
@@ -121,113 +31,72 @@ productos.forEach((producto) => {
         <div>
             <p class="talle"><b>talle:</b> ${producto.talle}</p>
         </div>
+
     </div>
 
-        <input type="button" value="COMPRAR" id="comprar" class="btnCarrito">`
+        <input type="button" value="COMPRAR" id="${producto.id}" class="btnCarrito">`
 
-    contenedor.appendChild(divProd);
+        contenedor.appendChild(divProd);
+    
+        const btnComprar = document.getElementById(`${producto.id}`);
 
-    const btnCarrito = document.getElementById("comprar");
-
-    btnCarrito.addEventListener("click", () => {
-        agregarAlCarrito(producto.id)
-    })
-
-});
-
-
-// Agregando a carrito
-
-let carrito = [];
-
-const agregarAlCarrito = (productoId) => {
-    const item = productos.find((producto) => producto.id === productoId);
-    carrito.push(item);
-    // console.log(carrito);
-    console.log(item);
+        btnComprar.addEventListener("click", () => {
+            agregarAlCarrito(producto.id);
+        })
+    });
 }
 
+mostrarProductos(productos);
 
+// agregando productos al carrito
 
+const agregarAlCarrito = (prodId) => {
+    const item = productos.find((prod) => prod.id === prodId)
+    carrito.push(item);
+    actualizarCarrito();
+    console.log(carrito);
+}
 
+// Carrito y actualizacion
 
+const actualizarCarrito = () => {
+    modal.innerHTML = ""
+    carrito.forEach((prod) => {
 
+        const divProdEnCarrito = document.createElement("div");
+        divProdEnCarrito.className = ("prodSeleccionado");
+        divProdEnCarrito.innerHTML = `
+        <p class="producto">${prod.nombre}</p>
+        <img src="${prod.img}" alt="${prod.nombre}" class="imgProdCarrito">
+        <p class="precio">precio: $ ${prod.precio}</p>
+        <p class="cantidad">cantidad: ${prod.cantidad}</p>
+        <button onclick = "eliminarDelCarrito(${prod.id})" class="btnEliminar">ELIMINAR</button>             
+        `
+        modal.appendChild(divProdEnCarrito);
+    });
 
+}
 
+// eliminar producto del carrito
 
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId) 
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    actualizarCarrito();
+}
 
+// filtro del navegador por categorias 
 
+let filtroPrendas = document.getElementsByClassName("btnFiltro");
 
+for (const filtro of filtroPrendas) {
+    filtro.addEventListener("click", filtrarCategoria);
+}
 
-
-
-
-
-
-// let prendaSeleccionada = " "
-// let objetoSeleccionado = " "
-// let cantidadSeleccionada = 0
-// let seguirComprando = true;
-// let subtotal = 0;
-
-// do {
-//     const productosNombre = productos.map(productos => productos.nombre);
-//     alert("Bienvenida a la tienda de Alaska.\nLas prendas en stock son :\n" +productosNombre.join(", "));
-//     prendaSeleccionada = prompt("Que prenda queres comprar?\n" + productosNombre.join(", ")).toLowerCase();
-//     objetoSeleccionado = productos.find(prod => prod.nombre.toLowerCase() == prendaSeleccionada);
-//     alert("El color es: " + objetoSeleccionado.color + "\nEl talle es: " + objetoSeleccionado.talle + "\nEl precio es: $" + objetoSeleccionado.precio);
-//     cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"));
-//     if (cantidadSeleccionada > objetoSeleccionado.cantidad) {
-//         alert("No contamos con el stock ingresado, por favor ingrese otra cantidad")
-//         cantidadSeleccionada = parseInt(prompt("Cuantos " + objetoSeleccionado.nombre + " queres comprar?"))
-//     } else {
-//         alert("Queres comprar " + cantidadSeleccionada + " " + objetoSeleccionado.nombre);
-//         subtotal += cantidadSeleccionada * objetoSeleccionado.precio;
-//         objetoSeleccionado.cantidad = cantidadSeleccionada;
-//         carrito.push(objetoSeleccionado);
-//     }
-
-//     seguirComprando = confirm("Queres seguir comprando?");
-
-//     if (seguirComprando == true) {
-        
-//     } else {
-//         mostrarCarrito();
-//         alert("El subtotal de tu compra es de: $"+subtotal);
-//     }
-
-// } while (seguirComprando);
-
-
-
-// function mostrarCarrito () {
-//     let mensaje = "Estos son los productos de tu carrito\n";
-//     carrito.forEach((producto) => {
-//         mensaje += producto.nombre+ ": "+producto.cantidad+"\n";
-//     })
-//     alert(mensaje);
-// }
-
-
-// let envio = prompt("Elegi ENVIO A DOMICILIO o RETIRAR POR EL LOCAL").toLowerCase()
-// let datosEnvio = ""
-// let datosRetiro = ""
-// let totalAbonar = subtotal + 2000
-
-// function Envio() {
-
-//     if ((envio == "envio a domicilio") && (subtotal >= 10000)) {
-//         alert("El envio a domicilio es gratis")
-//         alert("El total abonar es $" + subtotal)
-//         datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
-//     } else if ((envio == "envio a domicilio") && (subtotal < 10000)) {
-//         alert("El costo del envio a domicilio es de $2000")
-//         alert("El total abonar es $" + totalAbonar)
-//         datosEnvio = prompt("Ingresa nombre completo, direccion, localidad, codigo postal y telefono para realizar el envio")
-//     } else {
-//         datosRetiro = prompt("Tu pedido estara disponible para retirar en los proximos 10 dias habiles, te enviaremos un mensaje cuando este disponible. Dejanos tu nombre completo y telefono")
-//     }
-
-// }
-
-// Envio();
+ function filtrarCategoria(e) {
+    e.preventDefault()
+    const categoria = e.target.id;  
+    const resultado = productos.filter(producto => producto.categoria.includes(categoria));
+    mostrarProductos(resultado);
+}
