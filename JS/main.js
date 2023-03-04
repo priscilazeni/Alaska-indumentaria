41ae04f23a880714792f4f450def2eb1a77797c2
Priscila <priscilazeniquel@hotmail.com>
2023-03-04 18:15:52 -0300

    Tercer pre-entrega

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,26 @@
+const contenedor = document.getElementById("contenedor");
+const modal = document.getElementById("modal"); 
+
+let carrito = [];
+
+// abrir y cerrar ventana modal. NOTA (la ventana modal se cierra cuando hace click dentro de ella solo cuando no hay productos en el carrito, si hay productos no se cierra)
+
+const abrirCarrito = document.getElementById('carrito')
+
+
+abrirCarrito.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
 // Construccion de las Card
 
-const contenedor = document.getElementById("contenedor");
-const modal = document.getElementById("modal");
-let carrito = [];
 
 function mostrarProductos(array) {
 
@@ -48,16 +66,41 @@ function mostrarProductos(array) {
 
 mostrarProductos(productos);
 
+
 // agregando productos al carrito
 
 const agregarAlCarrito = (prodId) => {
-    const item = productos.find((prod) => prod.id === prodId)
-    carrito.push(item);
+
+    const existe = carrito.some(prod => prod.id === prodId)
+
+    if (existe) {
+        const prod = carrito.map(prod => {
+            if (prod.id === prodId) {
+                prod.cantidad++
+            }
+        })
+    } else  {
+        const item = productos.find((prod) => prod.id === prodId)
+        carrito.push(item);
+    }
+   
     actualizarCarrito();
-    console.log(carrito);
 }
 
-// Carrito y actualizacion
+// 
+
+
+// localStorage. NOTA (al agregar storage me produce errores. ej: cuando agrego dos veces el mismo producto me suma la cantidad pero si agrego un nuevo producto me toma la misma cantidad que el primer producto elegido)
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     if (localStorage.getItem('carrito')) {
+//         carrito = JSON.parse(localStorage.getItem('carrito'))
+//         actualizarCarrito()
+//     }
+// });
+
+
+// actualizacion carrito. NOTA (no logro poder agregar un div para poder insertar el subtotal, envio y el total. Cuando lo pongo cada vez que agrego un producto al carrito desaparece el div)
 
 const actualizarCarrito = () => {
     modal.innerHTML = ""
@@ -70,14 +113,17 @@ const actualizarCarrito = () => {
         <img src="${prod.img}" alt="${prod.nombre}" class="imgProdCarrito">
         <p class="precio">precio: $ ${prod.precio}</p>
         <p class="cantidad">cantidad: ${prod.cantidad}</p>
-        <button onclick = "eliminarDelCarrito(${prod.id})" class="btnEliminar">ELIMINAR</button>             
+        <button onclick = "eliminarDelCarrito()" class="btnEliminar">ELIMINAR</button>   
+
         `
         modal.appendChild(divProdEnCarrito);
+
+        // localStorage.setItem('carrito', JSON.stringify(carrito))
     });
 
 }
 
-// eliminar producto del carrito
+// eliminar producto del carrito . NOTA (al eliminar los productos elimina del ultimo elegido al primero, no el producto seleccionado)
 
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId) 
@@ -85,6 +131,7 @@ const eliminarDelCarrito = (prodId) => {
     carrito.splice(indice, 1)
     actualizarCarrito();
 }
+
 
 // filtro del navegador por categorias 
 
@@ -100,3 +147,6 @@ for (const filtro of filtroPrendas) {
     const resultado = productos.filter(producto => producto.categoria.includes(categoria));
     mostrarProductos(resultado);
 }
+
+// POR HACER :
+// poner precio, envio y total en el carrito.
