aec935df795febe4103e600ba9a74dd3cc81a529
Priscila <priscilazeniquel@hotmail.com>
2023-03-13 20:43:58 -0300

    Actualizacion final del proyecto

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,55 @@
 const contenedor = document.getElementById("contenedor");
-const modal = document.getElementById("modal"); 
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-// abrir y cerrar ventana modal. NOTA (la ventana modal se cierra cuando hace click dentro de ella solo cuando no hay productos en el carrito, si hay productos no se cierra)
+// abrir y cerrar ventana modal
 
-const abrirCarrito = document.getElementById('carrito')
+if (document.getElementById("carrito")){
+    const modal = document.getElementById("miModal");
+    const btn = document.getElementById("carrito");
+    const span = document.getElementsByClassName("cerrar")[0];
+    const body = document.getElementsByTagName("body")[0];
 
+    btn.onclick = function() { // se muestra ventana modal haciendo click en carrito
+        modal.style.display = "block";
 
-abrirCarrito.addEventListener('click', function() {
-    modal.style.display = 'block';
-});
-
-window.addEventListener('click', function(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
     }
-});
+
+    span.onclick = function() { // se oculta haciendo click en la X span
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(e) { // se cierra la ventana al hacer click por fuera de ella
+        if (e.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
+
+// fetch
+
+let productos = []; // array vacio que va a contener los productos a renderizar
+
+fetch('./JSON/data.json')
+.then((response) => response.json())
+.then((data) => {
+    productos = data;
+    mostrarProductos(productos);
+})
 
 
-// Construccion de las Card
-
+// renderizacion de las cards
 
 function mostrarProductos(array) {
 
@@ -36,35 +65,39 @@ function mostrarProductos(array) {
     </div>
 
     <div class="infoContenedor">
-
         <div class="infoUno">
-            <p class=""titulo>${producto.nombre}</p>
-            <p><b>$${producto.precio}</b></p>
+            <p class="titulo">${producto.nombre.toUpperCase()}</p>
+            <p "precio"><b>$${producto.precio}</b></p>
         </div>
 
         <div class="detalleContenedor">
-            <p class="color"><b>color:</b> ${producto.color}</p>
+            <p class="color"><b>COLOR:</b> ${producto.color.toUpperCase()}</p>
         </div>
 
         <div>
-            <p class="talle"><b>talle:</b> ${producto.talle}</p>
+            <p class="talle"><b>TALLE:</b> ${producto.talle.toUpperCase()}</p>
         </div>
-
     </div>
 
-        <input type="button" value="COMPRAR" id="${producto.id}" class="btnCarrito">`
+        <input type="button" value="AGREGAR" id="${producto.id}" class="btnCarrito">`
 
         contenedor.appendChild(divProd);
-    
+
         const btnComprar = document.getElementById(`${producto.id}`);
 
         btnComprar.addEventListener("click", () => {
             agregarAlCarrito(producto.id);
+            Toastify({ // libreria toastify que provee el alert que indica que se agrego un producto al carrito
+                text: "Agregaste un producto al carrito",
+                duration: 1500,
+                style: {
+                    background: 'rgba(7,7,7,7.2)',
+                },
+            }).showToast();
         })
     });
-}
 
-mostrarProductos(productos);
+}
 
 
 // agregando productos al carrito
@@ -73,67 +106,92 @@ const agregarAlCarrito = (prodId) => {
 
     const existe = carrito.some(prod => prod.id === prodId)
 
-    if (existe) {
-        const prod = carrito.map(prod => {
+    if (existe) { // si el producto esta en el carrito aunmenta su cantidad
+        carrito.map(prod => {
             if (prod.id === prodId) {
                 prod.cantidad++
             }
+            totalCarrito()
+            localStorage.setItem('carrito', JSON.stringify(carrito)) 
         })
-    } else  {
+
+    } else  { // si el producto no esta lo agrega al carrito
         const item = productos.find((prod) => prod.id === prodId)
         carrito.push(item);
+        totalCarrito()
+        localStorage.setItem('carrito', JSON.stringify(carrito)) 
     }
    
     actualizarCarrito();
 }
 
-// 
+// actualizacion carrito
 
+const modalContenido = document.getElementById('modalContenido');
+const total = document.getElementById('total');
 
-// localStorage. NOTA (al agregar storage me produce errores. ej: cuando agrego dos veces el mismo producto me suma la cantidad pero si agrego un nuevo producto me toma la misma cantidad que el primer producto elegido)
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     if (localStorage.getItem('carrito')) {
-//         carrito = JSON.parse(localStorage.getItem('carrito'))
-//         actualizarCarrito()
-//     }
-// });
-
-
-// actualizacion carrito. NOTA (no logro poder agregar un div para poder insertar el subtotal, envio y el total. Cuando lo pongo cada vez que agrego un producto al carrito desaparece el div)
-
-const actualizarCarrito = () => {
-    modal.innerHTML = ""
+const actualizarCarrito = () => { // renderiza por card seleccionada, agregada al carrito
+    modalContenido.innerHTML = ""
     carrito.forEach((prod) => {
 
         const divProdEnCarrito = document.createElement("div");
         divProdEnCarrito.className = ("prodSeleccionado");
         divProdEnCarrito.innerHTML = `
-        <p class="producto">${prod.nombre}</p>
         <img src="${prod.img}" alt="${prod.nombre}" class="imgProdCarrito">
-        <p class="precio">precio: $ ${prod.precio}</p>
-        <p class="cantidad">cantidad: ${prod.cantidad}</p>
-        <button onclick = "eliminarDelCarrito()" class="btnEliminar">ELIMINAR</button>   
+        <div class='prodInfo'
+            <p id="producto">${prod.nombre. toUpperCase()}</p>
+            <p class="precio">$${prod.precio}</p>
+            <p id='cantidad'>CANTIDAD: ${prod.cantidad}</p>
+            <button id='btnEliminar-${prod.id}' class='btnEliminar'>ELIMINAR</button>   
+        </div>
 
         `
-        modal.appendChild(divProdEnCarrito);
+        modalContenido.appendChild(divProdEnCarrito);
 
-        // localStorage.setItem('carrito', JSON.stringify(carrito))
+        let btnEliminar = document.getElementById(`btnEliminar-${prod.id}`)
+        btnEliminar.addEventListener('click', () => {
+            eliminarDelCarrito(prod.id)
+        })
+
     });
 
 }
 
-// eliminar producto del carrito . NOTA (al eliminar los productos elimina del ultimo elegido al primero, no el producto seleccionado)
+// eliminar producto del carrito 
+
 
 const eliminarDelCarrito = (prodId) => {
+    
     const item = carrito.find((prod) => prod.id === prodId) 
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1)
+    localStorage.setItem('carrito', JSON.stringify(carrito)) 
+    totalCarrito()
     actualizarCarrito();
+
+}
+
+// precio total y validacion del envio
+
+const totalAbonar = document.getElementById("total")
+const envio = document.getElementById("envio")
+let totalEnvio = 0
+let envioGratis = 8000
+
+const totalCarrito = () => {
+    const totalCarrito = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
+    totalAbonar.innerHTML = `TOTAL $${totalCarrito}`
+    if (totalCarrito < envioGratis) {
+        totalEnvio = totalCarrito * 0.20
+        envio.innerHTML = `ENVIO $${totalEnvio}`;
+        } else {
+        envio.innerHTML = `EL ENVIO ES GRATIS!`
+    }
 }
 
 
 // filtro del navegador por categorias 
+
 
 let filtroPrendas = document.getElementsByClassName("btnFiltro");
 
@@ -148,5 +206,9 @@ for (const filtro of filtroPrendas) {
     mostrarProductos(resultado);
 }
 
-// POR HACER :
-// poner precio, envio y total en el carrito.
+// al hacer click en el logo se vuelve al inicio donde figuran todas las prendas
+
+const logo = document.getElementById("logo")
+logo.addEventListener("click", () => {
+    mostrarProductos(productos);
+})
